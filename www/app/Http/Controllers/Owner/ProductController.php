d4cbaee3a845b02b7f6012a622a4d866d8e243ad
acaibawl <hagiya0719@gmail.com>
2025-10-09 00:01:39 +0900

    商品indexページ作成。EagerLoding対応

--- www/app/Http/Controllers/Owner/ProductController.php
+++ www/app/Http/Controllers/Owner/ProductController.php
@@ -3,16 +3,35 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
+use App\Models\Owner;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+        $this->middleware(function (Request $request, $next) {
+            $productId = $request->route()->parameter('product');
+            if (!is_null($productId)) {
+                $product = Product::findOrFail($productId);
+                if ($product->shop->owner_id !== Auth::id()) {
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $products = Owner::with(['shop.products.imageFirst'])->findOrFail(Auth::id())->shop->products;
+        return view('owner.products.index', compact('products'));
     }
 
     /**
