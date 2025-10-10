ee4e904bc659dee51edeb10a214ee3256a30b5d2
acaibawl <hagiya0719@gmail.com>
2025-10-10 15:32:49 +0900

    MicroModalを使った画像選択実装

--- www/resources/js/bootstrap.js
+++ www/resources/js/bootstrap.js
@@ -7,6 +7,11 @@
 import axios from 'axios';
 window.axios = axios;
 
+import MicroModal from 'micromodal';
+MicroModal.init({
+    disableScroll: true,
+});
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
