2039c2b05bb981ad02aefb2b2bd48aa110b9d9d5
acaibawl <hagiya0719@gmail.com>
2025-10-11 22:22:08 +0900

    Product destroy実装

--- www/app/Http/Controllers/Owner/ProductController.php
+++ www/app/Http/Controllers/Owner/ProductController.php
@@ -190,6 +190,14 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return redirect()
+            ->route('owner.products.index')
+            ->with([
+                'message' => '商品を削除しました。',
+                'status' => 'alert',
+            ]);
     }
 }
