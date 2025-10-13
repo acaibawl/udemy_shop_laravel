9ce47e90eabc4bce6778abf2428b74572c9ad2ca
acaibawl <hagiya0719@gmail.com>
2025-10-14 00:52:39 +0900

    stripeのcheckoutバックエンド作成

--- www/app/Http/Controllers/User/CartController.php
+++ www/app/Http/Controllers/User/CartController.php
@@ -7,6 +7,8 @@ use App\Models\Cart;
 use App\Models\User;
 use Auth;
 use Illuminate\Http\Request;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
 class CartController extends Controller
 {
@@ -50,5 +52,36 @@ class CartController extends Controller
             ->delete();
 
         return redirect()->route('user.cart.index');
+    }
+
+    public function checkout()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+
+        $lineItems = [];
+        foreach ($products as $product) {
+            $lineItem = [
+                'name' => $product->name,
+                'description' => $product->information,
+                'amount' => $product->price,
+                'currency' => 'jpy',
+                'quantity' => $product->pivot->quantity,
+            ];
+            $lineItems[] = $lineItem;
+        }
+
+        Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [$lineItems],
+            'mode' => 'payment',
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('user.cart.index'),
+        ]);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+
+        return view('user.checkout', compact('session', 'publicKey'));
     }
 }
